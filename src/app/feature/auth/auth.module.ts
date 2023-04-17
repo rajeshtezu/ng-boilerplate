1243bbcf59dff89ee28b4dc02ed5d4270241fcda
Rajesh Kumar <rajeshtezu90@gmail.com>
2023-04-17 13:18:37 +0530

    add login form

--- src/app/feature/auth/auth.module.ts
+++ src/app/feature/auth/auth.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRouterModule } from './auth-router/auth-router.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [CommonModule, AuthRouterModule],
+  declarations: [LoginComponent],
+  imports: [CommonModule, AuthRouterModule, FormsModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/feature/auth/login/login.component.ts
+++ src/app/feature/auth/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.loginFormInit();
   }
 
+  loginFormInit() {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  handleLogin(e: MouseEvent) {
+    e.preventDefault();
+
+    if (!this.loginForm.valid) {
+      Object.values(this.loginForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+
+      return;
+    }
+
+    const { email, password } = this.loginForm.value;
+
+    console.log('Login', email, password);
+  }
 }
